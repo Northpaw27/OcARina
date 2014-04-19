0fdfe373504d925280d321ffaf4353a45a47676b
Northpaw27 <vortex6@mac.com>
2014-04-19 22:51:14 +0100

    changed a little code

--- ocARino/ocARino.ino
+++ ocARino/ocARino.ino
@@ -18,7 +18,7 @@ void setup() {
   pinMode(23, INPUT_PULLUP);
   //LED pins
   pinMode(4,OUTPUT);
-  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
 
 }
 
@@ -31,23 +31,23 @@ void loop()
   
   //read touch pins and activate midi note
   int note;
-  while (touchRead(15) > 3000){
-    note = 62;
+  if (touchRead(17) > 3000){
+    note = 72;
   }
-  while (touchRead(16) > 3000){
-    note = 64;
-  }
-  while (touchRead(17) > 3000){
-    note = 66;
-  }
-  while (touchRead(18) > 3000){
-    note = 67;
-  }
-  while (touchRead(19) > 3000){
+  if (touchRead(18) > 3000){
     note = 69;
   }
-  while (touchRead(22) > 3000){
-    note = 72;
+  if (touchRead(22) > 3000){
+    note = 67;
+  }
+  if (touchRead(16) > 3000){
+    note = 66;
+  }
+  if (touchRead(15) > 3000){
+    note = 64;
+  }
+  if (touchRead(19) > 3000){
+    note = 62;
   }
 
 
@@ -57,13 +57,13 @@ void loop()
     usbMIDI.sendNoteOn(note ,analogRead(A0)/8, 2);
     //fade LEDs according to fan speed
     analogWrite(4,analogRead(A0)/4);
-    analogWrite(2,analogRead(A0)/4);
+    analogWrite(3,analogRead(A0)/4);
   }
   //turn note and LEDs off
   else {
     usbMIDI.sendNoteOff(note ,analogRead(A0)/8, 2);
     analogWrite(4,10);
-    analogWrite(2,10);
+    analogWrite(3,10);
   }
 
   delay(1);
